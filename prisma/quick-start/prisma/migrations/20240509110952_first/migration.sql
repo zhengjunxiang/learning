109e9266d046a8302bcbf8043accfe341fc784e4
zhengjunxiang <zhengjunxiang@hzrcb.com>
2024-05-10 18:23:59 +0800

    feat: 添加 nest和prisma

---
+++ prisma/quick-start/prisma/migrations/20240509110952_first/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `age` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
